243d0b35d52c2fc5dc667270c20d9230405c3b82
Sam Hollyer <rsamhollyer@gmail.com>
2020-12-07 11:57:26 -0600

    add stuff

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// write an app
+/* write an app
+- use git log to check the list of commits
+- git remote : remove git server. label it 'origin'
+- file structure is set how it should be for future projects
+
+
+
+ */
